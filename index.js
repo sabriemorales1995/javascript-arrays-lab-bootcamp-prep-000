5e457b3cde20e794e2473b6a39d1301269e4c664
Sabrie Morales <sabriemorales1995@gmail.com>
2018-07-01 23:13:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function deconstructivelyAppendKitten(name){
+  kittens.push(name)
+}
+
+function deconstructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
+
+function deconstructivelyRemoveLastKitten(){
+  kitten.pop();
+}
+
+function deconstructivelyRemoveFirstKitten(){
+  kitten.shift();
+}
+
+function appendKitten(name){
+  var temp = [...kittens, name];
+  return temp;
+}
+
+function prependKitten(name){
+  var temp = [name, ...kittens];
+  return temp;
+}
+
+function removeLastKitten(){
+  return kittens.slice(0,kittens.length-1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1);
+}
